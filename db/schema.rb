f6eab2b49a4a02ef8dded0488585a9c0bfe6b864
Hossein Shafiei <h.shafiei@ut.ac.ir>
2018-12-06 17:23:33 +0330

    1

--- db/schema.rb
+++ db/schema.rb
@@ -157,7 +157,7 @@ ActiveRecord::Schema.define(version: 20181205080600) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  limit: 255, default: "", null: false
+    t.string   "email",                  limit: 255
     t.string   "encrypted_password",     limit: 255, default: "", null: false
     t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
@@ -168,7 +168,6 @@ ActiveRecord::Schema.define(version: 20181205080600) do
     t.string   "username",               limit: 255
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
 
